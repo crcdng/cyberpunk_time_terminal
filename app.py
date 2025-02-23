d13124e72de4901d1e931ca030a29f409dc42c9c
Michael Straeubig <crcdng@users.noreply.huggingface.co>
2025-02-23 22:30:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer, get_current_time_in_timezone], ## add your tools here (don't remove final answer)
+    tools=[get_current_time_in_timezone], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
