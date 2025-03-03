873c0e959a27664a7acf0f4d61b20717b0bd622c
crcdng <crcdng@users.noreply.github.com>
2025-03-03 02:25:27 +0100

    fix

--- Gradio_UI.py
+++ Gradio_UI.py
@@ -420,7 +420,7 @@ class GradioUI:
                     1, 12, value=self.agent_get_steps(), step=1, label="Max. Number of Steps"
                 )
                 steps_input.change(self.agent_set_steps, steps_input, None)
-                selected = elf.agent_get_tools()
+                selected = self.agent_get_tools()
                 tools_list = gr.Dropdown(
                     choices=selected, 
                     value=selected,
